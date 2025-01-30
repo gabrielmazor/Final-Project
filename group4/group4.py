322c0594cd3dbfab41e2829949d1d97617c3811d
“Gabriel <gabimazor@gmail.com>
2025-01-31 00:22:29 +0200

    typo

--- group4/group4.py
+++ group4/group4.py
@@ -189,5 +189,5 @@ class Group4(SAONegotiator):
 
 # if you want to do a very small test, use the parameter small=True here. Otherwise, you can use the default parameters.
 if __name__ == "__main__":
-    from helpers.runner import run_a_tournament
+    from .helpers.runner import run_a_tournament
     run_a_tournament(Group4, small=False, debug=False)
