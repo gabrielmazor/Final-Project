61409b2b8300fc9570781d98c1c216c869901b41
“Gabriel <gabimazor@gmail.com>
2025-02-02 17:21:16 +0200

    building opponent classification, acceptance and bidding functions

--- group4/group4.py
+++ group4/group4.py
@@ -18,8 +18,15 @@ class Group4(SAONegotiator):
     """
 
     rational_outcomes = tuple()
+    opponent_rational_outcomes = tuple()
+    joint_outcomes = tuple()
 
-    partner_reserved_value = 0
+    opponent_reserved_value = 0.01
+    opponent_style = None
+    opponent_offers = []
+    lowest_offer = 1
+
+    exp = 1
 
     def on_preferences_changed(self, changes):
         """
@@ -33,15 +40,27 @@ class Group4(SAONegotiator):
         # If there a no outcomes (should in theory never happen)
         if self.ufun is None:
             return
+        
+        # Estimate the reservation value, as a first guess, the opponent has the same reserved_value as you
+        self.opponent_reserved_value = self.ufun.reserved_value
 
+        # Get the rational outcomes for both parties
         self.rational_outcomes = [
             _
             for _ in self.nmi.outcome_space.enumerate_or_sample()  # enumerates outcome space when finite, samples when infinite
             if self.ufun(_) > self.ufun.reserved_value
         ]
 
-        # Estimate the reservation value, as a first guess, the opponent has the same reserved_value as you
-        self.partner_reserved_value = self.ufun.reserved_value
+        self.opponent_rational_outcomes = [
+            _
+            for _ in self.nmi.outcome_space.enumerate_or_sample()  # enumerates outcome space when finite, samples when infinite
+            if self.opponent_ufun(_) > self.opponent_reserved_value
+        ]
+        
+        self.joint_outcomes = list(set(self.rational_outcomes) & set(self.opponent_rational_outcomes))
+        
+        # Sort rational outcomes by utility
+        self.joint_outcomes.sort(key=lambda o: self.ufun(o), reverse=True)
 
     def __call__(self, state: SAOState) -> SAOResponse:
         """
@@ -63,6 +82,9 @@ class Group4(SAONegotiator):
               - If this is `None`, you are starting the negotiation now (no offers yet).
         """
         offer = state.current_offer
+        
+        if state.relative_time < 0.5:
+            self.classify_opponent(state)
 
         self.update_partner_reserved_value(state)
 
@@ -85,10 +107,11 @@ class Group4(SAONegotiator):
         Returns: a bool.
         """
         assert self.ufun
+        tresh = self.ufun.reserved_value + (self.ufun(self.joint_outcomes[0]) - self.ufun.reserved_value) * (1-state.relative_time ** self.exp)
 
         offer = state.current_offer
 
-        if self.ufun(offer) > (2 * self.ufun.reserved_value):
+        if self.ufun(offer) > tresh:
             return True
         return False
 
@@ -100,9 +123,23 @@ class Group4(SAONegotiator):
         Returns: The counter offer as Outcome.
         """
 
+        if self.opponent_style == "Conceder":
+            options = [o for o in self.joint_outcomes if self.opponent_ufun(o) > self.lowest_offer]
+            options = [o for o in options if self.ufun(o) > self.opponent_ufun(o)]
+            if options:
+                return options[0]
+        elif self.opponent_style == "Boulware":
+            options = [o for o in self.joint_outcomes if abs(self.opponent_ufun(o) - self.lowest_offer) <= 0.05]
+            options = [o for o in options if self.ufun(o) > self.opponent_ufun(o)]
+            if options:
+                return options[0]
+            else:
+                options = [o for o in self.joint_outcomes if self.opponent_ufun(o) > self.lowest_offer]
+                options = [o for o in options if self.ufun(o) > self.opponent_ufun(o)]
+                if options:
+                    return options[0]
         # The opponent's ufun can be accessed using self.opponent_ufun, which is not used yet.
-
-        return random.choice(self.rational_outcomes)
+        return self.joint_outcomes[0]
 
     def update_partner_reserved_value(self, state: SAOState) -> None:
         """This is one of the functions you can implement.
@@ -114,20 +151,64 @@ class Group4(SAONegotiator):
 
         offer = state.current_offer
 
-        if self.opponent_ufun(offer) < self.partner_reserved_value:
-            self.partner_reserved_value = float(self.opponent_ufun(offer)) / 2
+        # Append offer to opponent offers
+        self.opponent_offers.append(self.opponent_ufun(offer))
+
+        # Update lowest offer
+        if self.opponent_ufun(offer) < self.lowest_offer:
+            self.lowest_offer = self.opponent_ufun(offer)
+
+        r = self.lowest_offer * state.relative_time * 0.6
+
+        if r >= self.opponent_reserved_value:
+            self.opponent_reserved_value = r
+        else:
+        # reserved value decreased, updating the rational outcomes
+            self.opponent_rational_outcomes = [
+                _
+                for _ in self.nmi.outcome_space.enumerate_or_sample()  # enumerates outcome space when finite, samples when infinite
+                if self.opponent_ufun(_) > self.opponent_reserved_value
+            ]
 
         # update rational_outcomes by removing the outcomes that are below the reservation value of the opponent
-        # Watch out: if the reserved value decreases, this will not add any outcomes.
-        rational_outcomes = self.rational_outcomes = [
+   
+        self.opponent_rational_outcomes = [
             _
-            for _ in self.rational_outcomes
-            if self.opponent_ufun(_) > self.partner_reserved_value
+            for _ in self.opponent_rational_outcomes
+            if self.opponent_ufun(_) > self.opponent_reserved_value
         ]
+        
+        self.joint_outcomes = list(set(self.rational_outcomes) & set(self.opponent_rational_outcomes))
+        
+        # if no intersection, set joint_outcomes to rational_outcomes
+        if not self.joint_outcomes:
+            self.joint_outcomes = self.rational_outcomes
+        
+        self.joint_outcomes.sort(key=lambda o: self.ufun(o), reverse=True)
+    
+
+    # Helper functions
+    def classify_opponent(self, state: SAOState) -> None:
+        if len(self.opponent_offers) < 10:
+            self.exp = 1
+            self.opponent_style = None
+        else:
+        # Calculate the slope of the opponent's offers         
+            slope = (self.opponent_offers[0] - self.opponent_offers[-1]) / state.relative_time
+
+            if slope < 0.015:
+                self.opponent_style = "Boulware"
+                self.exp = 1.3
+            else:
+                self.opponent_style = "Conceder"
+                self.exp = 0.6
+            
+
+
 
 
 # if you want to do a very small test, use the parameter small=True here. Otherwise, you can use the default parameters.
 if __name__ == "__main__":
     from .helpers.runner import run_a_tournament
 
-    run_a_tournament(Group4, small=True)
+    run_a_tournament(Group4, small=True, debug=True)
